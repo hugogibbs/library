08bc06a7b33abd8c3feb18f9d8cbb01dda90542d
Hugo Gibbs <hugo.d.gibbs@gmail.com>
2022-09-22 00:55:25 -0300

    .

--- script.js
+++ script.js
@@ -16,19 +16,36 @@ function Book(Title, Author, Pages, Read, Remove) {
 function addBookToLibrary(Title, Author, Pages, Read, Remove) {
     let book = new Book(Title, Author, Pages, Read, Remove);
     myLibrary.push(book);
+    displayBooksOnPage();
 }
+
 
 // display array in a table
 function displayBooksOnPage () {
-    const books = document.querySelector(".books");
+    
+// remove all previously displayed elements before loop over again
+    const removeTable = document.querySelectorAll(".tr");
+    console.log("show me the node count of current tr...", removeTable);
+    for (let i = 0; i < removeTable.length; i++) {
+        removeTable[i].remove();
+    }
 
+
+
+// loop over the library array and display elements in the table
+    let index = 0;
     myLibrary.forEach(myLibrary => {
     const row = document.createElement('tr');
     row.classList.add('tr');
+    row.id = "tr";
     table.appendChild(row);
+
+
+    
+
     for (let key in myLibrary) {
         console.log(`${key}: ${myLibrary[key]}`);
-        if (`${key}`== 'Read') {
+        
             const item = document.createElement('td');
             row.appendChild(item);
             const readBtn = document.createElement('button');
@@ -37,35 +54,78 @@ function displayBooksOnPage () {
             item.appendChild(readBtn);
         }
         else if (`${key}`== 'Remove'){
+            // Link the data attribute of the remove button to the array and row of the table
             const item = document.createElement('td');
             row.appendChild(item);
-            const remBtn = document.createElement('button');
-            remBtn.textContent = 'REMOVE';
-            remBtn.className = 'remove';
-            item.appendChild(remBtn);
+            const removeBookButton = document.createElement('button');
+            removeBookButton.className = 'remove';
+            removeBookButton.textContent = 'REMOVE';
+            removeBookButton.dataset.linkedArray = index;
+            index++;
+            console.log("show me the dataset link back to the array...", removeBookButton.dataset.linkedArray);
+            item.appendChild(removeBookButton);
+            removeBookButton.addEventListener("click", removeBookFromLibrary);
+
+            function removeBookFromLibrary() {
+            let retrieveBookToRemove = removeBookButton.dataset.linkedArray;
+            console.log("Attempting to remove array item via data attribute...", parseInt(retrieveBookToRemove));
+            myLibrary.splice(parseInt(retrieveBookToRemove), 1);
+            row.remove();
+            displayBooksOnPage();
+  
         }
+    }
         else {
             const item = document.createElement('td');
             item.textContent = (`${myLibrary[key]}`);
             row.appendChild(item);
         
         }
-    }
-    
 
-})
+        // Start event listener / remove array item from array and row from parent via data link
+        
+        }
+    
+    })
+}
+
+
+ 
+
+
+//transform form data to variables for intake
+
+function intakeFormData() {
+    event.preventDefault();
+    let Title = document.getElementById("book_title").value;
+    let Author = document.getElementById("book_author").value;
+    let Pages = document.getElementById("book_pages").value;
+    let Read = document.getElementById("read_yesno").value;
+    let Remove = 'Remove'
+
+    if ((Title == "")|| (Author =="") || (Pages=="")) {
+        alert("Please fill all the elements in the form");
+        return;
+    }
+
+    addBookToLibrary(Title, Author, Pages, Read, Remove);
+
+    document.getElementById("addBook").reset();
 
 }
 
-addBookToLibrary("Beat the system", "Hugo Gibbs", "123", "Read");
-addBookToLibrary("Rich Dad, Poor Dad", "Robert Kiyosaki", "231", "Not Read");
-addBookToLibrary("The 5 AM Club", "Robin Sharma", "321", "Read");
-addBookToLibrary("10X", "Grant Cardone", "222", "Not Read");
+
+addBookToLibrary("The Bible", "Inspired by God", "1200", "Read");
+addBookToLibrary("Unlimited Power", "Tony Robbins", "531", "Not Read");
+addBookToLibrary("Harry Potter", "J.K. Rowling", "221", "Read");
+
+
+//start event listener to add form input to library array
+const submitButton = document.Selector(".submit_book");
+submitButton.className = 'submitBook';
+submitButton.addEventListener('click', intakeFormData);
 
 console.log("End of code array contents", myLibrary);
-
-displayBooksOnPage();
-
 
 // show and hide form with the add book and X buttons 
 
@@ -77,3 +137,6 @@ document.getElementById('close').addEventListener('click', function(){
     document.querySelector (".popup").style.display = "none";
 })
 
+document.getElementById('submit_book').addEventListener('click', function(){
+    document.querySelector (".popup").style.display = "none";
+})
