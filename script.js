73c86bc8893f64881c289541d795f9f91b56f8c3
Hugo Gibbs <hugo.d.gibbs@gmail.com>
2022-09-23 18:12:59 -0300

    .

--- script.js
+++ script.js
@@ -3,15 +3,16 @@
 let myLibrary = [];
 
 // object constructor
-function Book(Title, Author, Pages) {
+function Book(Title, Author, Pages, Read) {
     this.Title = Title;
     this.Author = Author;
     this.Pages = Pages;
+    this.Read = Read;
 }
 
 // function for adding a new book to the array
-function addBookToLibrary(Title, Author, Pages) {
-    let book = new Book(Title, Author, Pages);
+function addBookToLibrary(Title, Author, Pages, Read) {
+    let book = new Book(Title, Author, Pages, Read);
     myLibrary.push(book);
     displayBooksOnPage();
 }
@@ -38,44 +39,43 @@ function displayBooksOnPage () {
 
         for (let key in myLibrarys) {
             console.log(`${key}: ${myLibrarys[key]}`);
-    
+
+                if (`${key}` == 'Read') {
+                const readItem = document.createElement('td');
+                row.appendChild(readItem);
+                const readButton = document.createElement('button');
+                readItem.append(readButton);
+                readButton.className = 'read';
+                readButton.id = 'read';
+                readButton.textContent = (`${myLibrarys[key]}`);
+
+                readButton.addEventListener('click', function(){
+                    if (readButton.textContent == 'READ') {
+                        readButton.textContent = 'NOT READ';
+                    }
+                    else if (readButton.textContent == 'NOT READ') {
+                        readButton.textContent = 'READ';
+                    }
+                })
+
+                }
+                else {
                 const item = document.createElement('td');
                 item.textContent = (`${myLibrarys[key]}`);
                 row.appendChild(item);
+                }
+            }
+        
+
+        document.getElementById('read').addEventListener('click', function(){
                 
+            if (document.getElementById('read').value == 'READ') {
+                readButton.textContent = 'NOT READ';
             }
-        
-        // Create read button
-        const readItem = document.createElement('td');
-        row.appendChild(readItem);
-        const readBookButton = document.createElement('button');
-        readBookButton.className = 'read';
-        readBookButton.textContent = 'READ';
-        
-        //Link the data attribute to the toggle read button to the array and table
-        readBookButton.dataset.linkedArray = index;
-        console.log("show me the dataset link back to the array FOR READ STATUS BUTTON...", readBookButton.dataset.linkedArray)
-        readItem.appendChild(readBookButton);
-       
-        // create event listener/toggle logic for array objects prototype for read status change
-
-
-        readBookButton.addEventListener("click", toggleReadStatus);
-
-        function toggleReadStatus() {
-            let retrieveBookToToggle = readBookButton.dataset.linkedArray;
-            Book.prototype = Object.create(Book.prototype);
-            const toggleBook = new Book();
-            console.log("What is the toggle initial value?...", myLibrary[parseInt(retrieveBookToToggle)].Read)
-
-            //Run check to see what read value is present to toggle from
-            if ((readBookButton.textContent) == "READ") {
-                readBookButton.textContent = "NOT READ";
+            else if (document.getElementById('read').value == 'NOT READ') {
+                readButton.textContent = 'READ';
             }
-            else if ((readBookButton.textContent) == "NOT READ") {
-                readBookButton.textContent = "READ";
-            }
-        }
+        })
 
         // Create remove book button and add class attribute for each array row
         const buttonItem = document.createElement('td');
@@ -113,8 +113,9 @@ function intakeFormData() {
     let Title = document.getElementById("book_title").value;
     let Author = document.getElementById("book_author").value;
     let Pages = document.getElementById("book_pages").value;
-    let Read = check_box.checked.value;
-    if (check_read.checked) {
+    let Read = document.getElementById("check_read").checked;
+    
+    if (document.getElementById("check_read").checked == true) {
         Read = 'READ';
     }
     else {
@@ -126,15 +127,15 @@ function intakeFormData() {
         return;
     }
 
-    addBookToLibrary(Title, Author, Pages);
+    addBookToLibrary(Title, Author, Pages, Read);
 
     document.getElementById("addBook").reset();
 
 }
 
-addBookToLibrary("The Bible", "Inspired by God", "1200");
-addBookToLibrary("Unlimited Power", "Tony Robbins", "531");
-addBookToLibrary("Harry Potter", "J.K. Rowling", "221");
+addBookToLibrary("The Bible", "Inspired by God", "1200", 'READ');
+addBookToLibrary("Unlimited Power", "Tony Robbins", "531", 'NOT READ');
+addBookToLibrary("Harry Potter", "J.K. Rowling", "221", "READ");
 
 
 //start event listener to add form input to library array
